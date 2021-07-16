cc3195437cb083557e2b2cfc79a8abf3dad8354b
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-16 11:40:43 -0300

    implementação com TypeScript completa

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,10 +2,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repository.css";
 import { useState, useEffect} from "react";
 
-
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
 export function RepositoryList(){
 
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
 
